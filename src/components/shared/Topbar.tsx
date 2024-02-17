63224a69f3415e661810fd2090217dcf5a584cf6
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-02-18 00:12:45 +0530

    18.02.2024

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from '../ui/button'
 
 const Topbar = () => {
   return (
     <section className='topbar'>
         <div className='flex-between py-4 px-5'>
             <Link to='/' className='flex gap-3 items-center'>
-                <img />
+                <img 
+                    src='/assets/images/logo.svg'
+                    alt='logo'
+                    width={130}
+                    height={325}
+                />
             </Link>
+
+            <div className='flex gap-4'>
+                <Button></Button>
+            </div>
         </div>
     </section>
   )
