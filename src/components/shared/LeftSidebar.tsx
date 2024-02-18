4bbc923be25482fed407bf0c5e0549b1f0bed51b
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-02-18 13:36:20 +0530

    18.02.2024

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Button } from '../ui/button'
+import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
+import { useUserContext } from '@/context/AuthContext'
 
 const LeftSidebar = () => {
+
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(0);
+    }
+  }, [isSuccess]);
+
   return (
-    <div>LeftSidebar</div>
+    <nav className='leftsidebar'>
+      <div className='flex flex-col gap-11'>
+        <Link to='/' className='flex gap-3 items-center'>
+          <img
+            src='/assets/images/logo.svg'
+            alt='logo'
+            width={170}
+            height={36}
+          />
+        </Link>
+
+        <Link to={`/profile/${user.id}`}>
+
+        </Link>
+      </div>
+    </nav>
   )
 }
 
