11030ac9e49afc55655034f3d3534ca95e0f4106
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-02-18 13:40:20 +0530

    18.02.2024

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -28,8 +28,20 @@ const LeftSidebar = () => {
           />
         </Link>
 
-        <Link to={`/profile/${user.id}`}>
-
+        <Link to={`/profile/${user.id}`} className='flex gap-3 items-center'>
+          <img
+             src={user.imageUrl || '/assets/icons/profile-placeholder.svg'}
+             alt='profile'
+             className='h-14 w-14 rounded-full'
+          />
+          <div className='flex flex-col'>
+            <p className='body-bold'>
+              {user.name}
+            </p>
+            <p className='small-regular text-light-3'>
+              @{user.username}
+            </p>
+          </div>
         </Link>
       </div>
     </nav>
