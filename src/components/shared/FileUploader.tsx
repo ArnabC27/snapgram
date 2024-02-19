7e8c8161a016171448e853d0a7d71a584cbde398
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-02-19 14:27:43 +0530

    19.02.2024

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -2,17 +2,22 @@ import React, { useCallback, useState } from 'react'
 import { FileWithPath, useDropzone } from 'react-dropzone'
 import { Button } from '../ui/button';
 
-const FileUploader = ({ fieldChange, mediaUrl }: any) => {
-    const [ file, setFile ] = useState([]);
-    const [ fileUrl, setFileUrl ] = useState('');
+type FileUploaderProps = {
+    fieldChange: (FILES: File[]) => void;
+    mediaUrl: string;
+}
 
-    const onDrop = useCallback((acceptedFiles: FileWithPath) => {
+const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+    const [file, setFile] = useState<File[]>([]);
+    const [fileUrl, setFileUrl] = useState('');
+
+    const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
         setFile(acceptedFiles);
         fieldChange(acceptedFiles);
         setFileUrl(URL.createObjectURL(acceptedFiles[0]));
     }, [file])
 
-    const { getRootProps, getInputProps } = useDropzone({ 
+    const { getRootProps, getInputProps } = useDropzone({
         onDrop,
         accept: {
             'image/*': ['.png', '.jpeg', '.jpg', '.svg']
@@ -24,12 +29,19 @@ const FileUploader = ({ fieldChange, mediaUrl }: any) => {
             <input {...getInputProps()} className='cursor-pointer' />
             {
                 fileUrl ? (
-                    <div>
-                        test 1
-                    </div>
+                    <>
+                        <div className='flex flex-1 justify-center w-full p-5 lg:p-10'>
+                            <img
+                                src={fileUrl}
+                                alt='image'
+                                className='file_uploader-img'
+                            />
+                        </div>
+                        <p className='file_uploader-label'>Click or drag photo to replace</p>
+                    </>
                 ) : (
                     <div className='file-uploader_box'>
-                        <img 
+                        <img
                             src='/assets/icons/file-upload.svg'
                             width={96}
                             height={77}
